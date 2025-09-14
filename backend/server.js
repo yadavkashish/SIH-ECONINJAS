bf78cc3f7d4495f85256719ba1fd6a925fd6fec3
Srishti Verma <srishtiverma358@gmail.com>
2025-09-14 11:55:12 +0530

    map-error resolved

--- backend/server.js
+++ backend/server.js
@@ -57,5 +57,6 @@ app.use("/api/locations", locationsRouter);
 
 const server = http.createServer(app);
 initWebSocket(server);
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`✅ Server + WS running on port ${PORT}`));
